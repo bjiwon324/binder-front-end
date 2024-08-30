a1dbca27476378c5d000b0372d9458ea671dc580
yunsusu <148840514+yunsusu@users.noreply.github.com>
2024-08-30 23:55:30 +0900

    [feat] 내 위치 정보 가져오기 구현

--- next.config.mjs
+++ next.config.mjs
@@ -10,4 +10,5 @@ export default withPWA({
   dest: "public", // PWA를 위한 service worker 파일이 생성될 경로
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // 개발 모드에서는 PWA 비활성화
 })(nextConfig);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,10 @@ import type { AppProps } from "next/app";
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-  <QueryClientProvider client={queryClient}>
-    <Component {...pageProps} />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
