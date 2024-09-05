3d4f82c7363e7a7e6e6c0d5c7527184f6fd12dd1
Jiwon <148836373+bjiwon324@users.noreply.github.com>
2024-09-05 23:54:13 +0900

    [Feat] nav title  구현
    * feat : Nav Title 구현

---
+++ src/pages/addbin/index.tsx
@@ -0,0 +1,13 @@
+import NavTitle from "@/components/commons/NavTitle";
+import styles from "./Addbin.module.scss";
+import classNames from "classnames/bind";
+
+const cn = classNames.bind(styles);
+
+export default function AddBin() {
+  return (
+    <>
+      <NavTitle>신규 위치 등록</NavTitle>
+    </>
+  );
+}
