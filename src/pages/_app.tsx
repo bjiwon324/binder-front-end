ac8dda6774e4a09fb2753f2cbb62450f11341f01
yunsusu <rladbstn1212@gmail.com>
2024-09-12 19:50:35 +0900

    feat : 구글 애널리틱스 연결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import Gnb from "@/components/commons/Gnb";
+import "@/styles/base/index.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
-import "@/styles/base/index.scss";
-import Gnb from "@/components/commons/Gnb";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,24 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      {/* Google Analytics 및 Kakao SDK 스크립트 추가 */}
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.min.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZEWF6HWTJB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZEWF6HWTJB');
+        `}
+      </Script>
+
       <div style={{ backgroundColor: "gray", height: "100vh" }}>
         <div style={{ width: "390px", margin: "0 auto" }}>
           <Component {...pageProps} />
