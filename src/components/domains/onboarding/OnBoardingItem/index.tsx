c76bbc1144a27a6abd0c3eb751ce6ea2aef92120
yunsusu <rladbstn1212@gmail.com>
2024-09-19 22:25:28 +0900

    feat : 이미지에 따라 사이즈 다르게

--- src/components/domains/onboarding/OnBoardingItem/index.tsx
+++ src/components/domains/onboarding/OnBoardingItem/index.tsx
@@ -11,24 +11,37 @@ interface OnBoardingItem {
   next: () => void;
 }
 
+// 이미지 크기를 계산하는 함수
+const getImageSize = (title: string) => {
+  if (title === "근처 쓰레기통을 찾아보세요") {
+    return { width: 200, height: 427 };
+  } else if (title === "위치 동의 받고 빠르게 안내 받아보세요") {
+    return { width: 270, height: 427 };
+  } else {
+    return { width: 140, height: 427 };
+  }
+};
+
 export default function OnBoardingItem({
   title,
-  subTitle = " ",
+  subTitle,
   imgs,
   next,
 }: OnBoardingItem) {
+  const imageSize = getImageSize(title); // 이미지 사이즈 가져오기
+
   return (
     <div className={cn("wrap")}>
       <div className={cn("titleWrap")}>
         <h2>{title}</h2>
         <p>{subTitle}</p>
       </div>
-      {title !== "위치 동의 받고 빠르게 안내 받아보세요" ? (
-        <Image src={imgs} alt="온보딩 이미지" width={200} height={427} />
-      ) : (
-        <Image src={imgs} alt="온보딩 이미지" width={270} height={427} />
-      )}
-
+      <Image
+        src={imgs}
+        alt="온보딩 이미지"
+        width={imageSize.width}
+        height={imageSize.height}
+      />
       <div className={cn("nextBtn")} onClick={next}>
         다음
       </div>
