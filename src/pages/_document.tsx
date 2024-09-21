ab8b02471ea5043068fcd67178e0179c3ac40a62
yunsusu <rladbstn1212@gmail.com>
2024-09-21 22:22:08 +0900

    feat : meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,25 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <title>주변 쓰레기통 찾기, Binder </title>
+        <link rel="icon" href="/images/logo-144.png" />
+
+        {/* Open Graph 메타 태그 */}
+        <meta property="og:title" content="Binder" />
+        <meta property="og:description" content="내 주변 쓰레기통을 찾아보자" />
+        <meta property="og:image" content="/images/logo-144.png" />
+        <meta property="og:url" content="https://www.bin-finder.net/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter 메타 태그 */}
+        <meta name="twitter:card" content="/images/logo-144.png" />
+        <meta name="twitter:title" content="Binder" />
+        <meta
+          name="twitter:description"
+          content="내 주변 쓰레기통을 찾아보자"
+        />
+        <meta name="twitter:image" content="/images/logo-144.png" />
+
         <link rel="manifest" href="/manifest.json" />
         <script
           defer
