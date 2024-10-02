520c8a5394557c70412997b11f42f18270a9b994
yunsusu <rladbstn1212@gmail.com>
2024-10-02 10:50:47 +0900

    fix : 무한스크롤을 위한 ref 수정

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,7 +1,7 @@
 import SearchInput from "@/components/domains/search/SearchInput";
 import SearchItems from "@/components/domains/search/SearchItems";
 import SearchToggle from "@/components/domains/search/SearchToggle";
-import { searchDetailList } from "@/lib/atoms/atom";
+import { searchDetailList, searchToggle } from "@/lib/atoms/atom";
 import { useAtom } from "jotai";
 import { useEffect, useRef, useState } from "react";
 
@@ -9,16 +9,26 @@ export default function Search() {
   const [detail, setDetail] = useAtom(searchDetailList);
   const [prevSearchPick, setPrevSearchPick] = useState<string>("");
   const target = useRef<HTMLDivElement>(null);
+  const [btnState] = useAtom(searchToggle);
   useEffect(() => {
     setDetail(null);
   }, []);
+
   return (
     <>
       <SearchInput prevSearchPick={prevSearchPick} />
       <SearchToggle target={target} />
 
       <SearchItems setPrevSearchPick={setPrevSearchPick} target={target} />
-      <div ref={target} style={{ height: "1px", marginTop: "-5rem" }}></div>
+
+      <div
+        ref={target}
+        style={{
+          height: "1px",
+          marginTop: "-5rem",
+          display: btnState === "저장한 장소" ? "block" : "none",
+        }}
+      ></div>
     </>
   );
 }
